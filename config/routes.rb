1691af25460ebfac0253ab3f45b4026e3da25bf4
Aki Suzuki <aki@siliconsurfer.io>
2016-02-05 20:34:46 -0800

    added devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
+  devise_for :users
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1 do
+    end
+
+  end
 
 end
