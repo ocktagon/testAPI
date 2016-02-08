93a20172c03021fbc8d0ff663a7be9d963583337
Aki Suzuki <aki@siliconsurfer.io>
2016-02-07 23:39:57 -0800

    Configured shoulda-matchers correctly.  Tests running

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
+
 describe User do
-  # before { @user = FactoryGirl.build(:user) }
-  # subject { @user }
-  # it { should respond_to(:email) }
-  # it { should respond_to(:password) }
-  # it { should respond_to(:password_confirmation) }
-  # it { should be_valid }
-#
+  before { @user = FactoryGirl.build(:user) }
+  subject { @user }
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should be_valid }
+
+
+  describe "when email is not present" do
+    before { @user.email = "" }
+    subject { @user }
+    it { should validate_presence_of(:email) }
+    # it { should validate_uniqueness_of(:email) }
+    it { should validate_confirmation_of(:password) }
+    it { should allow_value('example@domain.com').for(:email) }
+  end
+
   # it { should validate_uniqueness_of(:auth_token)}
   #   it "generates a unique token" do
   #     Devise.stub(:friendly_token).and_return("auniquetoken123")
