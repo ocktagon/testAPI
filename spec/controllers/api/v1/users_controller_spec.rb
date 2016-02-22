2ea0da7270801e1316bbd6298822276467018448
Aki Suzuki <aki@siliconsurfer.io>
2016-02-22 00:07:41 -0800

    user patch/destroy complete, no rspec errors

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -65,17 +65,25 @@ RSpec.describe Api::V1::UsersController, type: :controller do
         @user = FactoryGirl.create :user
         patch :update, { id: @user.id,
                          user: { email: "bademail.com" } }, format: :json
-  end
+      end
       it "renders an errors json" do
         user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response).to have_key(:errors)
       end
-      it "renders the json errors on whye the user could not be created" do
-      ￼￼￼￼￼      user_response = JSON.parse(response.body, symbolize_names: true)
+      it "renders the json errors on why the user could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response[:errors][:email]).to include "is invalid"
       end
       it { should respond_with 422 }
     end
   end
+
+describe "DELETE #destroy" do
+  before(:each) do
+    @user = FactoryGirl.create :user
+    delete :destroy, { id: @user.id }, format: :json
+  end
+  it { should respond_with 204 }
+end
 
 end
